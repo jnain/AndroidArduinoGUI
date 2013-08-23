8216e318ab2dda4070b353a5e65dca9cbce17650
Jonathan Nain <jonathan.nain@gmail.com>
2013-08-23 15:26:51 +0200

    working for led 13 HIGh and LOW

---
+++ src/main/java/org/sintef/jarduino/AndroidJArduinoGUI.java
@@ -0,0 +1,243 @@
+package org.sintef.jarduino;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+import org.sintef.jarduino.comm.AndroidBluetooth4JArduino;
+import org.sintef.jarduino.comm.AndroidBluetoothConfiguration;
+
+import java.io.IOException;
+import java.util.*;
+
+public class AndroidJArduinoGUI extends Activity {
+
+    private static String TAG = "android.gui";
+
+    /**
+     * Called when the activity is first created.
+     * @param savedInstanceState If the activity is being re-initialized after 
+     * previously being shut down then this Bundle contains the data it most 
+     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
+     */
+    private String mUUID = "00001101-0000-1000-8000-00805F9B34FB"; //Special code, do not
+    //change unless you know what you're doing.
+
+    private String deviceName = "FireFly-4101";
+    private int REQUEST_ENABLE_BT = 2000; //What you want here.
+    List<Button> buttons = new ArrayList<Button>();
+    static final int CUSTOM_DIALOG_ID = 0;
+    ListView dialog_ListView;
+    String[] listContent = {
+            "Digital HIGH",
+            "Digital LOW",
+            "Digital READ",
+            "Analog READ"};
+    Map<String, AnalogPin> analog = new Hashtable<String, AnalogPin>(){{
+        put("pinA0", AnalogPin.A_0);
+        put("pinA1", AnalogPin.A_1);
+        put("pinA2", AnalogPin.A_2);
+        put("pinA3", AnalogPin.A_3);
+        put("pinA4", AnalogPin.A_4);
+        put("pinA5", AnalogPin.A_5);
+    }};
+    Map<String, DigitalPin> digital = new Hashtable<String, DigitalPin>(){{
+        put("pin2", DigitalPin.PIN_2);
+        put("pin3", DigitalPin.PIN_3);
+        put("pin4", DigitalPin.PIN_4);
+        put("pin5", DigitalPin.PIN_5);
+        put("pin6", DigitalPin.PIN_6);
+        put("pin7", DigitalPin.PIN_7);
+        put("pin8", DigitalPin.PIN_8);
+        put("pin9", DigitalPin.PIN_9);
+        put("pin10", DigitalPin.PIN_10);
+        put("pin11", DigitalPin.PIN_11);
+        put("pin12", DigitalPin.PIN_12);
+        put("pin13", DigitalPin.PIN_13);
+    }};
+    private String clickedButton = null;
+
+    private GUIController mController = null;
+
+    /**
+     * Called when the activity is first created.
+     * @param savedInstanceState If the activity is being re-initialized after
+     * previously being shut down then this Bundle contains the data it most
+     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        initButtons();
+
+        final BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+        }
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        BluetoothDevice mmDevice = null;
+
+        List<String> mArray = new ArrayList<String>();
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        // If there are paired devices
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in a ListView
+                if(device.getName().equals(deviceName))
+                    mmDevice = device;
+                mArray.add(device.getName() + "\n" + device.getAddress());
+            }
+        }
+
+        //Creating the socket.
+        BluetoothSocket mmSocket;
+        BluetoothSocket tmp = null;
+
+        UUID myUUID = UUID.fromString(mUUID);
+        try {
+            // MY_UUID is the app's UUID string, also used by the server code
+            tmp = mmDevice.createRfcommSocketToServiceRecord(myUUID);
+        } catch (IOException e) { }
+        mmSocket = tmp;
+
+        //socket created, try to connect
+
+        try {
+            mmSocket.connect();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+
+        mController = new GUIController();
+        AndroidBluetooth4JArduino device = new AndroidBluetooth4JArduino(new AndroidBluetoothConfiguration(mmSocket));
+        mController.register(device);
+        device.register(mController);
+
+    }
+
+    void initButtons(){
+        buttons.add(((Button) findViewById(R.id.pin10)));
+        buttons.add(((Button) findViewById(R.id.pin11)));
+        buttons.add(((Button) findViewById(R.id.pin12)));
+        buttons.add(((Button) findViewById(R.id.pin13)));
+        buttons.add(((Button) findViewById(R.id.pin2)));
+        buttons.add(((Button) findViewById(R.id.pin3)));
+        buttons.add(((Button) findViewById(R.id.pin4)));
+        buttons.add(((Button) findViewById(R.id.pin5)));
+        buttons.add(((Button) findViewById(R.id.pin6)));
+        buttons.add(((Button) findViewById(R.id.pin7)));
+        buttons.add(((Button) findViewById(R.id.pin8)));
+        buttons.add(((Button) findViewById(R.id.pin9)));
+        buttons.add(((Button) findViewById(R.id.pinA0)));
+        buttons.add(((Button) findViewById(R.id.pinA0)));
+        buttons.add(((Button) findViewById(R.id.pinA1)));
+        buttons.add(((Button) findViewById(R.id.pinA2)));
+        buttons.add(((Button) findViewById(R.id.pinA3)));
+        buttons.add(((Button) findViewById(R.id.pinA4)));
+        buttons.add(((Button) findViewById(R.id.pinA5)));
+
+        for(final Button b : buttons){
+            b.setOnClickListener(new Button.OnClickListener(){
+                public void onClick(View arg0) {
+                    clickedButton = b.getText().toString();
+                    showDialog(CUSTOM_DIALOG_ID);
+                }
+            });
+        }
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+
+        Dialog dialog = null;
+
+        switch(id) {
+            case CUSTOM_DIALOG_ID:
+                dialog = new Dialog(this);
+                dialog.setContentView(R.layout.dialoglayout);
+                dialog.setTitle("Action");
+
+                dialog.setCancelable(true);
+                dialog.setCanceledOnTouchOutside(true);
+
+                dialog.setOnCancelListener(new DialogInterface.OnCancelListener(){
+                    public void onCancel(DialogInterface dialog) {
+                        // TODO Auto-generated method stub
+                        /*Toast.makeText(AndroidJArduinoGUI.this,
+                                "OnCancelListener",
+                                Toast.LENGTH_LONG).show();*/
+                    }
+                });
+
+                dialog.setOnDismissListener(new DialogInterface.OnDismissListener(){
+                    public void onDismiss(DialogInterface dialog) {
+                        // TODO Auto-generated method stub
+                        /*Toast.makeText(AndroidJArduinoGUI.this,
+                                "OnDismissListener",
+                                Toast.LENGTH_LONG).show();*/
+                    }
+                });
+
+                //Prepare ListView in dialog
+                dialog_ListView = (ListView)dialog.findViewById(R.id.dialoglist);
+                ArrayAdapter<String> adapter
+                        = new ArrayAdapter<String>(this,
+                        R.layout.item, listContent);
+                dialog_ListView.setAdapter(adapter);
+                dialog_ListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+                    public void onItemClick(AdapterView<?> parent, View view,
+                                            int position, long id) {
+                        String pin = clickedButton;
+                        switch(position){
+                            case 0:
+                                mController.senddigitalWrite(digital.get(pin), DigitalState.HIGH);
+                                break;
+                            case 1:
+                                mController.senddigitalWrite(digital.get(pin), DigitalState.LOW);
+                                break;
+                            case 2:
+                                mController.senddigitalRead(digital.get(pin));
+                                break;
+                            case 3:
+                                mController.sendanalogRead(analog.get(pin));
+                                break;
+                        }
+
+                        dismissDialog(CUSTOM_DIALOG_ID);
+                    }
+                });
+
+                break;
+        }
+        return dialog;
+    }
+
+    @Override
+    protected void onPrepareDialog(int id, Dialog dialog, Bundle bundle) {
+        // TODO Auto-generated method stub
+        super.onPrepareDialog(id, dialog, bundle);
+        switch(id) {
+            case CUSTOM_DIALOG_ID:
+                //
+                break;
+        }
+
+    }
+
+}
+
---
+++ src/main/java/org/sintef/jarduino/GUIController.java
@@ -0,0 +1,123 @@
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Authors: Jan Ole Skotterud Franck Fleurey and Brice Morin
+ * Company: SINTEF IKT, Oslo, Norway
+ * Date: 2011
+ */
+package org.sintef.jarduino;
+
+import android.util.Log;
+import org.sintef.jarduino.comm.AndroidBluetooth4JArduino;
+import org.sintef.jarduino.observer.JArduinoClientObserver;
+import org.sintef.jarduino.observer.JArduinoClientSubject;
+import org.sintef.jarduino.observer.JArduinoObserver;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+public class GUIController implements JArduinoObserver, JArduinoClientSubject {
+
+    private List<JArduinoClientObserver> handlers;
+    private SimpleDateFormat dateFormat;
+    private JArduino mJArduino;
+    private String TAG = "GUIController";
+
+    public GUIController(){
+        handlers = new LinkedList<JArduinoClientObserver>();
+        dateFormat = new SimpleDateFormat("dd MMM yyy 'at' HH:mm:ss.SSS");
+    }
+
+    private void doSend(FixedSizePacket data){
+        if (data != null) {
+            Log.d(TAG, data+" --> "+data.getPacket());
+            for (JArduinoClientObserver h : handlers){
+                h.receiveMsg(data.getPacket());
+            }
+        }
+        else {
+            Log.d(TAG, "Data is null");
+        }
+    }
+
+    public final void sendpinMode(PinMode mode, DigitalPin pin) {
+        FixedSizePacket fsp = null;
+        fsp = JArduinoProtocol.createPinMode(pin, mode);
+        doSend(fsp);
+    }
+
+    public final void senddigitalRead(DigitalPin pin) {
+        FixedSizePacket fsp = null;
+        fsp = JArduinoProtocol.createDigitalRead(pin);
+        doSend(fsp);
+    }
+
+    public final void senddigitalWrite(DigitalPin pin, DigitalState value) {
+        FixedSizePacket fsp = null;
+        fsp = JArduinoProtocol.createDigitalWrite(pin, value);
+        doSend(fsp);
+        //digitalWrite(DigitalPin.PIN_13, DigitalState.LOW);
+    }
+
+    public final void sendanalogRead(AnalogPin pin) {
+        FixedSizePacket fsp = null;
+        fsp = JArduinoProtocol.createAnalogRead(pin);
+        doSend(fsp);
+    }
+
+    public final void sendanalogWrite(PWMPin pin, byte value) {
+        FixedSizePacket fsp = null;
+        fsp = JArduinoProtocol.createAnalogWrite(pin, value);
+        doSend(fsp);
+    }
+
+    public final void sendping() {
+        doSend(JArduinoProtocol.createPing());
+    }
+
+    public final void receiveMessage(byte[] packet){
+        FixedSizePacket data = JArduinoProtocol.createMessageFromPacket(packet);
+
+        if (data != null) {
+            //gui.writeToLog( " ["+dateFormat.format(new Date(System.currentTimeMillis()))+"]: "+data.toString()+" --> "+FixedSizePacket.toString(packet));
+            Log.d(TAG, " [" + dateFormat.format(new Date(System.currentTimeMillis())) + "]: " + data.toString() + " --> " + FixedSizePacket.toString(packet));
+            //TODO Add
+        }
+    }
+
+    //Methods defined in the Observer pattern specific to JArduino
+    public void receiveMsg(byte[] msg) {
+        receiveMessage(msg);
+    }
+
+    public void register(JArduinoClientObserver observer) {
+        handlers.add(observer);
+    }
+
+    public void unregister(JArduinoClientObserver observer) {
+        handlers.remove(observer);
+    }
+
+    public void unregisterAll(){
+        AndroidBluetooth4JArduino temp;
+        for (int i = 0; i < handlers.size(); i++){
+            temp = (AndroidBluetooth4JArduino) handlers.get(i);
+            Log.d(TAG, "Closer " + temp);
+            //temp.close();
+        }
+        handlers.clear();
+        Log.d(TAG, "Size = " + handlers.size());
+    }
+}
